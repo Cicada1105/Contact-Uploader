91dbac3a1332d1922aff5f9b5fbf8820ee5ff734
Cicada1105 <guitarlegen@gmail.com>
2018-12-02 11:39:34 -0800

    including header

--- load-table.php
+++ load-table.php
@@ -1,5 +1,5 @@
 <?php
-    //header("Content-Type: application/json; charset=UTF-8");
+    header("Content-Type: application/json; charset=UTF-8");
 	$file = $fopen("contacts.json","r") or die("The file cannot be found");
 	$fileString = $fread($file,filesize("contacts.json"));
 	fclose($file);
