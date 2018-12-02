68eefb5889d70ee865c5ee6d18dfc6df6f1da7a2
Cicada1105 <guitarlegen@gmail.com>
2018-12-01 19:25:20 -0800

    Added phpinfo

--- load-table.php
+++ load-table.php
@@ -1,4 +1,5 @@
 <?php
+	phpinfo();
     //header("Content-Type: application/json; charset=UTF-8");
 	$file = $fopen("contacts.json","r") or die("The file cannot be found");
 	$fileString = $fread($file,filesize("contacts.json"));
